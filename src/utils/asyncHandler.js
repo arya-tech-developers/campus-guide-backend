6811910ece511aa4837a4f369db950627b59c2d3
Deepanshu Saini <saini1234deepanshu@gmail.com>
2024-01-10 20:44:03 +0530

    Modified src/utils/asyncHandler.js ---> code asynchandler in try/catch format

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,17 @@
-const asyncHandler = (requestHandler) =>(req,res,next)=>{
-    Promise.resolve(requestHandler(request,res,next)).reject((err)=>next(err))
-}
+// const asyncHandler = (requestHandler) =>(req,res,next)=>{
+//     Promise.resolve(requestHandler(request,res,next)).reject((err)=>next(err))
+// }
 
+
+const asyncHandler = async (requestHandler)=>{
+    try{
+        await requestHandler(req,res,next);
+    }catch(error){
+        res.status(error.code).json({
+            succes:false,
+            message:error.message
+        })
+    }
+}
 
 export {asyncHandler};
