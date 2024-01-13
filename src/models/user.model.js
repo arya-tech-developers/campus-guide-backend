3c1df9dc06e217dc0182cd13e53a552fd0a5dc90
Deepanshu Saini <saini1234deepanshu@gmail.com>
2024-01-13 21:35:59 +0530

    password hashing functionality, generate refresh and access token method added in userSchema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema({
     name:{
@@ -45,5 +47,38 @@ const userSchema = mongoose.Schema({
     }
 },{timestamps:true});
 
+
+userSchema.pre("save", async function(next){
+    if(this.isModified("password"))
+        this.password = bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await  bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        name:this.name,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
+    },
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id:this._id,    
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
 
 export const User = mongoose.model("User",userSchema);
