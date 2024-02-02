913527417a658a16991e8b196d15366a3f41df72
Deepanshu Saini <saini1234deepanshu@gmail.com>
2024-02-02 23:36:23 +0530

    added new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import { dbtest } from "../controllers/test.controller.js";
-
+import { logoutUser,loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import verifyJWT from "../middlewares/authorization.middleware.js";
 
 const router = Router()
 
-router.route("/registration").post(registerUser);
 
+router.route("/registration").post(registerUser)
+router.route("/login").get(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refresh-session").post(refreshAccessToken)
 
 export default router;
+
