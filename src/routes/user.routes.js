042c30a1b3a1b6256687e2787de42ce49c9b63ec
Deepanshu Saini <saini1234deepanshu@gmail.com>
2024-02-06 14:30:29 +0530

    added user refresh session routes and upload question paper route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { logoutUser,loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { uploadQuestionPaper } from "../controllers/questionPaper.controller.js";
 import verifyJWT from "../middlewares/authorization.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
@@ -11,6 +13,9 @@ router.route("/login").get(loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-session").post(refreshAccessToken)
+
+// resources routes
+router.route("/upload-question-paper").post(upload.single("questionPaper"),verifyJWT,uploadQuestionPaper)
 
 export default router;
 
