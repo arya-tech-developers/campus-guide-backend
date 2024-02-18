4494775ba9dd10176e44a8a8a49dd54de338320a
Deepanshu Saini <saini1234deepanshu@gmail.com>
2024-02-18 16:12:38 +0530

    all changes commit to upload code in personal repo

---
+++ src/models/questionPaper.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import { User } from "./user.model.js";
+
+
+const questionPaperSchema = await mongoose.Schema({
+    url: {
+        type: String,   //from cloudinary
+        // requried:true,
+    },
+    format: {
+        type: String,
+    },
+    subject: {
+        type: String,
+    },
+    subjectCode: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    paperYear: {
+        type: Date,
+        required: true
+    },
+    course: {
+        type: String,
+        required: true,
+        trim: true,
+        // required:true,
+        default: "b.tech",
+        lowercase: true
+    },
+    branch: {
+        type: String,
+        required: true,
+        trim: true,
+        enum: ["cse", "me", "ece", "ee", "ce"],
+        lowercase: true
+    },
+    semester: {
+        type: Number,
+        required: true,
+        index: true,
+        enum: [1, 2, 3, 4, 5, 6, 7, 8]
+    },
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    }
+}, { timestamps: true })
+
+
+
+export const QuestionPaper = await mongoose.model("QuestionPaper", questionPaperSchema);
