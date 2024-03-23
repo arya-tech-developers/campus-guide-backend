f17d219e651a514da30fa3f66576f20ddc84595c
Deepanshu Saini <89999194+geekydeepanshu@users.noreply.github.com>
2024-03-23 20:47:02 +0530

    Create fileUpload.js

---
+++ src/utils/fileUpload.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const fileUpload = async (localFilePath) => {
+    try {
+        if (localFilePath) {
+            const response = await cloudinary.uploader.upload(localFilePath,{ resource_type:"auto" });
+            console.log(response);
+            fs.unlinkSync(localFilePath);
+            return response;
+        }
+        return null;
+    }catch(error){
+        fs.unlinkSync(localFilePath);
+        console.log("Error:: ",error);
+        return null;
+    }
+  
+}
+
+export {fileUpload};
